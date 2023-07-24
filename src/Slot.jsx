1af54f3e5dce445e9b2de20a9cb5fc7c8be49ec3
Krishan Kant Sen <skrishan111213@gmail.com>
2023-07-24 13:50:18 +0530

    Update

--- src/Slot.jsx
+++ src/Slot.jsx
@@ -5,11 +5,14 @@ let x=props.x;
 let y=props.y;
 let z=props.z;
 if(x===y & y===z ){
+    alert("Matched");
  return(
+    
     <>
         <div className=" slot_inner">
             <h1>{x}{y}{z}</h1>
             <h1>This is Matching</h1>
+            
             <hr/>
         </div>
     </>
